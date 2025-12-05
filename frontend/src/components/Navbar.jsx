9cb9a54f153d7263e593ccc9c1d0eb2cc6f9168b
Dev Arya <darshanarya092@gmail.com>
2025-12-05 13:43:47 +0530

    Done!

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <>   
+      <div className='max-w-screen-2x1 mx-auto container px-6 md:px-40 shadow-lg h-16 fixed'>
+      <div className='flex justify-between'>
+          <h1 className='text-2xl cursor-pointer font-bold'>
+            Word<span className='text-3xl text-green-500'>To</span>PDF
+          </h1>
+          <h1 className='mt-1 text-2xl cursor-pointer font-bold hover:scale-125 duration-200'>Home</h1>
+      </div>
+      </div>
+    </>
+
+  )
+}
+
+export default Navbar
+
